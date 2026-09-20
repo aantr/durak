30a93e0ae47463de6a89ef5fe1ba8b90f0e40ea4
aantr <xgamestudio8@gmail.com>
2026-09-20 15:43:22 +0500

    dataset

--- detect.py
+++ detect.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import cv2
 from ultralytics import YOLO
 
 from constants import DETECTION_ENGINE_PATH
@@ -7,14 +8,14 @@ from constants import DETECTION_ENGINE_PATH
 model = YOLO(str(DETECTION_ENGINE_PATH))
 
 # Источник: путь к картинке, папке, видео или 0 для вебки
-source = "screenshots/IMG_1407.PNG"
+source = "screenshots/IMG_1408.PNG"
 
 results = model.predict(
     source=source,
     imgsz=1280,        # ВАЖНО: должно совпадать с тем, на чём экспортировали engine
-    conf=0.25,
+    conf=0.15,
     iou=0.45,
-    save=True,         # сохранить в runs/detect/predict*/
+    save=False,         # сохранить в runs/detect/predict*/
     show=False,         # показать окно (в headless не сработает)
     line_width=2,
     show_labels=True,
@@ -30,3 +31,15 @@ for r in results:
         conf = float(b.conf[0])
         name = r.names[cls]
         print(f"{name} {conf:.2f} {xyxy.tolist()}")
+
+SCALE = 0.5   # в 2 раза меньше
+
+for r in results:
+    img = r.plot(line_width=2)
+    h, w = img.shape[:2]
+    small = cv2.resize(img, (int(w * SCALE), int(h * SCALE)), interpolation=cv2.INTER_AREA)
+
+    cv2.imshow("YOLO", small)
+    cv2.waitKey(0)
+
+cv2.destroyAllWindows()
